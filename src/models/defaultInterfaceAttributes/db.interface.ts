8bce3125db0242b957c48d2b70ca9b4cbff63d26
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-11-19 10:31:29 +0900

    20201119 Update Interface Attribute
    기본 인터페이스와 속성을 한 폴더에 묶음

--- src/models/defaultInterfaceAttributes/db.interface.ts
+++ src/models/defaultInterfaceAttributes/db.interface.ts
@@ -1,7 +1,8 @@
 import Sequelize from "sequelize";
-import { UserInterface } from "./model.interfaces"
+import { UserInterface, ConsumerInterface } from "./model.interfaces"
 
 export interface DbInterface {
   sequelize: Sequelize.Sequelize;
   User: Sequelize.ModelCtor<UserInterface>;
+  Consumer: Sequelize.ModelCtor<ConsumerInterface>;
 }
