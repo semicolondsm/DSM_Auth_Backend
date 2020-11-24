2211cda3bb9cc63999871079417d31d205b3abae
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-11-25 08:10:37 +0900

    20201125 Update verifyToken Middleware
    verifyToken middleware 테스트 완료

--- PublicAPI/src/middleware/verifyToken/verifyToken.ts
+++ PublicAPI/src/middleware/verifyToken/verifyToken.ts
@@ -1,25 +1,21 @@
 import jwt from "jsonwebtoken";
 
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import { VerifyTokenRequest } from "./verifyToken.interface";
 
 const verifyToken = (req: VerifyTokenRequest, res: Response, next: NextFunction) => {
   try {
-    const token = req.headers["access-token"] as string;
+    const token: any = req.headers["access-token"];
     if(!token) {
       return res.status(400).json({
         code: 400, 
         message: "Bad Request",
       });
     }
-    if(req.decoded === undefined) {
-      return;
-    } else {
-      req.decoded = jwt.verify(token, process.env.JWT_SECRET!);
-      next();
-    }
+    req.decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    next();
   } catch(err) {
-    console.error(err);
+    //console.error(err);
     if(err.name === "TokenExpiredError") {
       return res.status(401).json({
         code: 419,
