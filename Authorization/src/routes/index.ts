0654a9fa161c218a3b933bbfdc1c2b0f61b8b339
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-11-25 19:01:11 +0900

    20201125 Update routers
    라우터 로직 refactoring

--- Authorization/src/routes/index.ts
+++ Authorization/src/routes/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 
 import userRegistrationRouter from "./userRegistration";
 import consumerRouter from "./consumer";
