0114353533d99a833ef76762c0dd89a8805b22f4
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-09 22:15:41 +0900

    Create dsmLogin api

---
+++ Authorization/src/controller/authentication.ts
@@ -0,0 +1,39 @@
+import { BusinessLogic } from "../middleware/businessLogicInterface";
+import { db } from "../models/index";
+import { UserInterface, ConsumerInterface } from "../models/defaultInterfaceAttributes/model.interfaces";
+import { HttpError } from "../middleware/errorHandler/customError";
+import bcrypt from "bcrypt";
+import redisClient from "../redisClient";
+
+const dsmLogin: BusinessLogic = async (req, res, next) => {
+
+  const { id, password, redirect_url, client_id } = req.body;
+  const promise_exUser: Promise<UserInterface | null> = db.User.findOne({ where: { identity: id } });
+  const promise_exConsumer: Promise<ConsumerInterface | null> = db.Consumer.findOne({ where: { client_id } });
+
+  // user authentication 
+  const exUser: UserInterface | null = await promise_exUser;
+  if(!exUser) {
+    throw new HttpError(401, "Unauthorized id");
+  } 
+  const pwdCmp: boolean = await bcrypt.compare(password, exUser.password as string);
+  if(!pwdCmp) {
+    throw new HttpError(401, "Unauthorized password");
+  }
+  
+  // consumer authenticaion
+  const exConsumer: ConsumerInterface | null = await promise_exConsumer;
+  if(!exConsumer || exConsumer.redirect_url !== redirect_url) {
+    throw new HttpError(400, "Bad Request");
+  } 
+  const authcode: string = (Math.random() * 1000000).toString();
+  redisClient.set(client_id, authcode);
+
+  res.status(200).json({
+    location: `${redirect_url}?authcode=${authcode}`,
+  });
+}
+
+export {
+  dsmLogin,
+}
