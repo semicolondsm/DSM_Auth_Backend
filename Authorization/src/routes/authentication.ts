66fc7244bed515d26f15513cb7214da508992b17
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-09 22:23:04 +0900

    Create login Rouer

--- Authorization/src/routes/authentication.ts
+++ Authorization/src/routes/authentication.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import * as dsmAuthentication from "../controller/authentication";;
+import { errorHandler } from "../middleware/errorHandler/errorHandler";
+import { BusinessLogic } from "../middleware/businessLogicInterface";
 
-const router = express.Router();
+const router: express.Router = express.Router();
+
+const dsmLoginRouter: BusinessLogic = errorHandler(dsmAuthentication.dsmLogin);
+
+router.post("/login", dsmLoginRouter);
 
 export default router;
