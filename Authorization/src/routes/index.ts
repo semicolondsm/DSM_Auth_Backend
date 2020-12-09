a2b5289bd449a7a26bc63219efbd5dc8a14ca57a
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-09 20:04:55 +0900

    Update filename
    file name 수정

--- Authorization/src/routes/index.ts
+++ Authorization/src/routes/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
-import userRegistrationRouter from "./userRegistration";
-import consumerRouter from "./consumer";
+import userRegistrationRouter from "./userService";
+import consumerRouter from "./consumerService";
 import authenticationRouter from "./authentication";
 
 const router: express.Router = express.Router();
