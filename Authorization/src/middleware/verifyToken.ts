e56067666bd5f559937de6a4f702fd7b721e4c17
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-12 20:42:15 +0900

    20201212 Update VerifyToken Middleware

--- Authorization/src/middleware/verifyToken.ts
+++ Authorization/src/middleware/verifyToken.ts
@@ -6,11 +6,11 @@ import { BusinessLogic } from "./businessLogicInterface";
 
 const verifyTokenLogic: BusinessLogic = (req, res, next) => {
   try {
-    const token: any = req.headers["access-token"];
+    const token: string | undefined = req.headers["access-token"] as string; // undifinedable
     if(!token) {
       throw new HttpError(400, "Bad Request", 400);
     }
-    req.decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    req.decoded = jwt.verify(token.slice(7), process.env.JWT_SECRET!);
     next();
   } catch(err) {
     console.error(err);
