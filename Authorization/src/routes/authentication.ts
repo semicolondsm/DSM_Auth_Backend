1c8513fa3c6effc2008a3458070c63f19598bba6
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-17 14:45:14 +0900

    20201217 Update Refresh Token Middleware
    라우터 설정

--- Authorization/src/routes/authentication.ts
+++ Authorization/src/routes/authentication.ts
@@ -3,6 +3,8 @@ import * as dsmAuthentication from "../controller/dsmAuthentication";;
 import { errorHandler } from "../middleware/errorHandler/errorHandler";
 import { BusinessLogic } from "../middleware/businessLogicInterface";
 
+import { verifyRefreshToken } from "../middleware/verifyRefreshToken";
+
 const router: express.Router = express.Router();
 
 const dsmLoginRouter: BusinessLogic = errorHandler(dsmAuthentication.dsmLogin);
@@ -11,6 +13,6 @@ const refreshTokenRouter: BusinessLogic = errorHandler(dsmAuthentication.refresh
 
 router.post("/login", dsmLoginRouter);
 router.post("/token", provideTokenRouter);
-router.get("/refresh", refreshTokenRouter);
+router.get("/refresh", verifyRefreshToken, refreshTokenRouter);
 
 export default router;
