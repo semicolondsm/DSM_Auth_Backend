36d844c350228747a88a8ec3ea621b40048f9008
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-03 22:43:29 +0900

    UPDATE; dsmauth router
    변경사항 적용

--- Authorization/src/routes/authentication.ts
+++ Authorization/src/routes/authentication.ts
@@ -4,6 +4,8 @@ import { errorHandler } from "../middleware/errorHandler/errorHandler";
 import { BusinessLogic } from "../middleware/businessLogicInterface";
 
 import { verifyRefreshToken } from "../middleware/verifyRefreshToken";
+import { validationRequest } from "../middleware/validationParameter";
+import { DsmAuthenticationSchema } from "../dto/dsmAuthentication.dto";
 
 const router: express.Router = express.Router();
 
@@ -11,7 +13,7 @@ const dsmLoginRouter: BusinessLogic = errorHandler(dsmAuthentication.dsmLogin);
 const provideTokenRouter: BusinessLogic = errorHandler(dsmAuthentication.provideToken);
 const refreshTokenRouter: BusinessLogic = errorHandler(dsmAuthentication.refreshToken);
 
-router.post("/login", dsmLoginRouter);
+router.post("/login", validationRequest(DsmAuthenticationSchema), dsmLoginRouter);
 router.post("/token", provideTokenRouter);
 router.get("/refresh", verifyRefreshToken, refreshTokenRouter);
 
