1d65591925c33d58c2b1de393770f538d63fedaf
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-03 22:51:24 +0900

    UPDATE; server.ts
    uncaughtException handle

--- Authorization/src/server.ts
+++ Authorization/src/server.ts
@@ -3,3 +3,7 @@ import app from "./app";
 app.listen(app.get("port"), () => {
   console.log("server on ", app.get("port"));
 });
+
+process.on("uncaughtException", (err: Error) => {
+  console.log(err);
+});
