0a89d5fe8695f9fa079fde7349b5052409db7f37
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-04 18:56:20 +0900

    RENAME; validationRequest.ts

--- Authorization/src/middleware/validationRequest.ts
+++ Authorization/src/middleware/validationRequest.ts
@@ -1,11 +1,11 @@
 import Joi from "joi"
-import { BusinessLogic } from "../middleware/businessLogicInterface";
+import { BusinessLogic } from "./businessLogicInterface";
 
 type ValidationRequest = <T>(schema: Joi.ObjectSchema<T>) => BusinessLogic;
 
 const validationRequest: ValidationRequest = <T>(schema: Joi.ObjectSchema<T>) => (req, res, next) => {
   const { error } = schema.validate(req.body);
-  error ? res.status(400).json({ message: "Bad Request" }) : next();
+  error ? res.status(400).json({ code: 400, message: "Bad Request" }) : next();
 }
 
 export { validationRequest }
